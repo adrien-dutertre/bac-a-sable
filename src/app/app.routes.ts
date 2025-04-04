d4df8eb7b34d0fc55648a1eb10865b4e246832c2
Adrien <adridu90@hotmail.fr>
2025-04-04 21:58:27 +0200

    modification du nom du dossier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { CalculatriceComponent } from './pages/calculatrice/calculatrice.component';
 import { PasswordGeneratorComponent } from './pages/password-generator/password-generator.component';
-import { ToDoComponent } from './pages/to-do/to-do.component';
+import { ToDoComponent } from './pages/todo/todo.component';
 
 export const routes: Routes = [
     { path: '', component : AccueilComponent },
