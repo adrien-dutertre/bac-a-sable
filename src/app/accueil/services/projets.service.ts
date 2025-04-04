1d00e5b3b15f05262505f63715b832fb4d426d19
Adrien <adridu90@hotmail.fr>
2025-04-04 21:52:34 +0200

    modification de la valeur de l'entete

--- src/app/accueil/services/projets.service.ts
+++ src/app/accueil/services/projets.service.ts
@@ -24,7 +24,7 @@ export class ProjetsService {
       route: "generateur-mdp"
     },
     {
-      titre: "To-do List",
+      titre: "To do List",
       description: "Si comme mon père vous faites des listes tout le temps, vous pouvez maintenant les faire aussi là.",
       couleur: "#fefae0",
       pattern:`url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23ccd5ae' fill-opacity='0.4'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E")`,
