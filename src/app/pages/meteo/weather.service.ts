ef922b4ebf186b3f422eb07c3237c91021849f80
Adrien <adridu90@hotmail.fr>
2025-09-15 14:01:42 +0200

    Préparation maj angular 20

--- src/app/pages/meteo/weather.service.ts
+++ src/app/pages/meteo/weather.service.ts
@@ -12,8 +12,8 @@ export class WeatherService {
 
   private http = inject(HttpClient);
   readonly linkAPI = "https://api.open-meteo.com/v1/forecast?daily=weather_code,temperature_2m_max,temperature_2m_min&models=meteofrance_seamless&current=is_day,temperature_2m,weather_code&timezone=Europe%2FBerlin&forecast_days=3";
-  readonly weatherCodePath = "/weather/weatherCode.json";
-  readonly citiesPath = "/weather/cities.json";
+  readonly weatherCodePath = "weather/weatherCode.json";
+  readonly citiesPath = "weather/cities.json";
   
   get(latitude: number = 45.7485, longitude: number = 4.8467): Observable<Weather> {
     return this.http.get<Weather>(this.linkAPI+`&latitude=${latitude}&longitude=${longitude}`);
